5e148b89b53b0739dc0febee087aa272a72f94bf
Omar Qurashi <96021536+OmarQurashi868@users.noreply.github.com>
2025-02-17 18:44:45 +0400

    svelcordot config, readme encoding

--- Svelcordot/src/lib/config.ts
+++ Svelcordot/src/lib/config.ts
@@ -1,6 +1,6 @@
 export const config = {
     CLIENT_ID: "0000000000000000000", // Discord Application ID
-    COMPANY_NAME: "TheSleepyKoala", // Company or Developer Name
-    PRODUCT_NAME: "Pong with GDScript", // Game Name
-    PRODUCT_VERSION: "0.1.0", // Game Version
+    COMPANY_NAME: "OmarQurashi868", // Company or Developer Name
+    PRODUCT_NAME: "marboota", // Game Name
+    PRODUCT_VERSION: "0.0.1", // Game Version
 };
